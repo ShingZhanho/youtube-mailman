cff7ce46826419dab1f4680cc04603e13b83b91e
Zhanho Shing <zhanhoshing@icloud.com>
2020-10-31 21:50:45 +0800

    added reset count & prevent heroku from breaking

--- run.py
+++ run.py
@@ -10,8 +10,19 @@ import random
 api = pyyoutube.Api(api_key=os.getenv('YOUTUBE_API_TOKEN'))  # initializes a new api object
 last_check = None
 print('YouTube mailman is now working...')
+reset_count = 0
 
 while True:
+    # reset_count is designed for not exceeding google cloud quota and not to trigger heroku 60 seconds error
+    if reset_count != 180:
+        reset_count += 1
+        print("\rReset count: " + str(reset_count) + " out of 180", end='')
+        utils.wait_for_ten_mins()
+        continue
+    else:
+        print("Reached reset count, now check for videos.")
+        reset_count = 0
+
     if last_check is None:  # wait for the first time
         last_check = time.time()
         # utils.wait_for_ten_mins()
@@ -20,11 +31,15 @@ while True:
     # gets the list of channels to check
     channelIDs = json.loads(os.getenv('CHANNELS_LIST'))
     for channel in channelIDs['channelIDs']:
-        results = api.search(parts='snippet', channel_id=channel, order='date')
+        try:
+            results = api.search(parts='snippet', channel_id=channel, order='date')
+        except pyyoutube.PyYouTubeException:
+            print('An error occurred while getting videos')
+            break
 
         # get latest video and compare
         video_upload_time = time.mktime(datetime.datetime.strptime(
-                results.items[0].snippet.publishedAt, '%Y-%m-%dT%H:%M:%SZ').timetuple())
+            results.items[0].snippet.publishedAt, '%Y-%m-%dT%H:%M:%SZ').timetuple())
 
         if video_upload_time > last_check:  # new video since last check
             # get a pattern for message
@@ -37,17 +52,16 @@ while True:
             video_link = 'https://youtu.be/' + results.items[0].id.videoId
 
             # generate the message
-            message = chosen_pattern.replace('[ChannelName]', channel_title)\
-                .replace('[VideoTitle]', video_title)\
+            message = chosen_pattern.replace('[ChannelName]', channel_title) \
+                .replace('[VideoTitle]', video_title) \
                 .replace('[VideoLink]', video_link)
             print("傳送了一條新的訊息：" + message)
 
             # make http request
             requests.post(os.getenv('WEBHOOK_URL'),
-                          data={"username": "YouTube mailman",
-                                "avatar_url": os.getenv('BOT_AVATAR_URL'),
+                          data={"username": "YouTube mailman", "avatar_url": os.getenv('BOT_AVATAR_URL'),
                                 "content": message})
     last_check = time.time()
-    print('在' + str(last_check) + '完成了一次檢查')
+    print('A check has been performed at ' + str(last_check))
     # print(results.items[0].snippet.title)
     utils.wait_for_ten_mins()
